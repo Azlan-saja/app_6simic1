750bcaf9b5c170eef3322b86c6e5e0aa47a7f86d
Azlan-saja <azlansaja19@gmail.com>
2024-02-13 18:41:32 +0700

    widget

--- lib/login.dart
+++ lib/login.dart
@@ -5,6 +5,20 @@ class MyLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hey,'),
+            const Text('Login Now!'),
+            const TextField(),
+            const TextField(),
+            ElevatedButton(onPressed: () {}, child: const Text('Login Now')),
+          ],
+        ),
+      ),
+    );
   }
 }
